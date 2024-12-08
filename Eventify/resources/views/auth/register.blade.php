85b625b4734bad8b2eb72c2e7e9d06902e121cac
Karla-A03 <karlaboytes03@gmail.com>
2024-12-07 18:21:51 -0600

    Se agregan roles a usuario

--- Eventify/resources/views/auth/register.blade.php
+++ Eventify/resources/views/auth/register.blade.php
@@ -39,6 +39,15 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+        <div class="mb-4">
+            <label for="role" class="block text-sm font-medium text-gray-700">Role</label>
+            <select id="role" name="role" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                <option value="asistente" {{ old('role') == 'asistente' ? 'selected' : '' }}>Asistente</option>
+                <option value="organizador" {{ old('role') == 'organizador' ? 'selected' : '' }}>Organizador</option>
+            </select>
+        </div>
+
+
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
